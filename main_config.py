7411ce1f98e610c21591f96b2ec1548b2205592d
anissa.manai <anissa.manai@stud.tu-darmstadt.de>
2022-06-29 23:41:38 +0200

    remove files

--- main_config.py
+++
@@ -1,5 +0,0 @@
-main_config = {
-    "train_dir": "C:\\Users\\aniss\\OneDrive\\Dokumente\\Uni\\Semester 4\\RBC\\RBC-Agent\\data\\RBC-data-2021-train.hdf5",
-    "val_dir": "C:\\Users\\aniss\\OneDrive\\Dokumente\\Uni\\Semester 4\\RBC\\RBC-Agent\\data\\RBC-data-2021-val.hdf5",
-    "test_dir": "C:\\Users\\aniss\\OneDrive\\Dokumente\\Uni\\Semester 4\\RBC\\RBC-Agent\\data\\RBC-data-2021-test.hdf5"
-}
