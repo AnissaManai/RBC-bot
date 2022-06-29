9ede0c108e407267a1e237a3eab83036b3b857dc
anissa.manai <anissa.manai@stud.tu-darmstadt.de>
2022-06-30 00:31:39 +0200

    Update main_config.py

--- train/main_config.py
+++ train/main_config.py
@@ -1,5 +1,5 @@
 main_config = {
-    "train_dir": "C:\\Users\\aniss\\OneDrive\\Dokumente\\Uni\\Semester 4\\RBC\\RBC-Agent\\data\\RBC-data-2021-train.hdf5",
-    "val_dir": "C:\\Users\\aniss\\OneDrive\\Dokumente\\Uni\\Semester 4\\RBC\\RBC-Agent\\data\\RBC-data-2021-val.hdf5",
-    "test_dir": "C:\\Users\\aniss\\OneDrive\\Dokumente\\Uni\\Semester 4\\RBC\\RBC-Agent\\data\\RBC-data-2021-test.hdf5"
+    "train_dir": "C:\\Users\\aniss\\OneDrive\\Dokumente\\Uni\\Semester4\\RBC\\RBC-Agent\\data\\RBC-data-2021-train.hdf5",
+    "val_dir": "C:\\Users\\aniss\\OneDrive\\Dokumente\\Uni\\Semester4\\RBC\\RBC-Agent\\data\\RBC-data-2021-val.hdf5",
+    "test_dir": "C:\\Users\\aniss\\OneDrive\\Dokumente\\Uni\\Semester4\\RBC\\RBC-Agent\\data\\RBC-data-2021-test.hdf5"
 }
