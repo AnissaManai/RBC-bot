d35be5804dd649f6e7f7a4cae49e9ffa70a39523
anissa.manai <anissa.manai@stud.tu-darmstadt.de>
2022-07-14 13:30:55 +0200

    Use a0_resnet from PommerLearn

---
+++ nn/a0_resnet.py
@@ -0,0 +1,110 @@
+"""
+This file is based on a0_resnet.py  from https://gitlab.com/jweil/PommerLearn
+
+"""
+
+import torch
+import torch.nn as nn
+from torch.nn import Sequential, Conv2d, BatchNorm2d, ReLU, LeakyReLU, Sigmoid, Tanh, Linear, Hardsigmoid, Hardswish, Module
+
+from nn.builder_util import get_act, _Stem, _PolicyHead
+
+
+def init_weights(m):
+    if isinstance(m, nn.Linear):
+        nn.init.kaiming_normal_(m.weight.data, nonlinearity="relu")
+
+
+class ResidualBlock(torch.nn.Module):
+    """
+    Definition of a residual block without any pooling operation
+    """
+
+    def __init__(self, channels, act_type):
+        """
+        :param channels: Number of channels used in the conv-operations
+        :param act_type: Activation function to use
+        """
+        super(ResidualBlock, self).__init__()
+        self.act_type = act_type
+
+        self.body = Sequential(Conv2d(in_channels=channels, out_channels=channels, kernel_size=(3, 3), padding=(1, 1), bias=False),
+                               BatchNorm2d(num_features=channels),
+                               get_act(act_type),
+                               Conv2d(in_channels=channels, out_channels=channels, kernel_size=(3, 3), padding=(1, 1), bias=False),
+                               BatchNorm2d(num_features=channels),
+                               get_act(act_type))
+
+    def forward(self, x):
+        """
+        Implementation of the forward pass of the residual block.
+        Uses a broadcast add operation for the shortcut and the output of the residual block
+        :param x: Input to the ResidualBlock
+        :return: Sum of the shortcut and the computed residual block computation
+        """
+        return x + self.body(x)
+
+
+class AlphaZeroResnet(Module):
+    """ Creates the alpha zero net description based on the given parameters."""
+
+    def __init__(
+        self,
+        n_labels=37,
+        channels=256,
+        nb_input_channels=21,
+        board_height=8,
+        board_width=8,
+        channels_policy_head=2,
+        num_res_blocks=19,
+        act_type="relu",
+        select_policy_from_plane=False,
+    ):
+        """
+        :param n_labels: Number of labels the for the policy
+        :param channels: Used for all convolution operations. (Except the last 2)
+        :param channels_policy_head: Number of channels in the bottle neck for the policy head
+        :param channels_value_head: Number of channels in the bottle neck for the value head
+        :param num_res_blocks: Number of residual blocks to stack. In the paper they used 19 or 39 residual blocks
+        :param value_fc_size: Fully Connected layer size. Used for the value output
+        :return: net description
+        """
+
+        super(AlphaZeroResnet, self).__init__()
+        self.nb_input_channels = nb_input_channels
+        self.board_height = board_height
+        self.board_width = board_width
+
+        res_blocks = []
+        for i in range(num_res_blocks):
+            res_blocks.append(ResidualBlock(channels, act_type))
+
+        self.body = Sequential(_Stem(channels=channels, act_type=act_type,
+                                     nb_input_channels=nb_input_channels),
+                               *res_blocks)
+
+        # create the heads which will be used in the fwd pass
+        self.policy_head = _PolicyHead(board_height, board_width, channels, channels_policy_head, n_labels,
+                                       act_type, select_policy_from_plane)
+
+    def forward(self, flat_input):
+        """
+        Implementation of the forward pass of the full network
+        Uses a broadcast add operation for the shortcut and the output of the residual block
+        :param x: Input to the ResidualBlock
+        :return: Policy Output
+        """
+        # input shape processing
+        # x, state_bf = self.unflatten(flat_input)
+        batch_size = flat_input.shape[0]
+        x = flat_input.view(batch_size, self.nb_input_channels, self.board_height, self.board_width)
+
+        out = self.body(x)
+
+        policy = self.policy_head(out)
+
+        return policy
+
+    def get_state_shape(self, batch_size: int):
+        raise NotImplementedError
+
---
+++ nn/builder_util.py
@@ -0,0 +1,96 @@
+"""
+Source: https://gitlab.com/jweil/PommerLearn
+
+Utility methods for building the neural network architectures.
+"""
+
+import math
+import torch
+from torch.nn import Sequential, Conv1d, Conv2d, BatchNorm2d, ReLU, LeakyReLU, Sigmoid, Tanh, Linear, Hardsigmoid, Hardswish,\
+    Module, AdaptiveAvgPool2d, BatchNorm1d
+
+def get_act(act_type):
+    """Wrapper method for different non linear activation functions"""
+    if act_type == "relu":
+        return ReLU()
+    if act_type == "sigmoid":
+        return Sigmoid()
+    if act_type == "tanh":
+        return Tanh()
+    if act_type == "lrelu":
+        return LeakyReLU(negative_slope=0.2)
+    if act_type == "hard_sigmoid":
+        return Hardsigmoid()
+    if act_type == "hard_swish":
+        return Hardswish()
+    raise NotImplementedError
+
+
+class _Stem(torch.nn.Module):
+    def __init__(self, channels, act_type="relu", nb_input_channels=34):
+        """
+        Definition of the stem proposed by the alpha zero authors
+        :param channels: Number of channels for 1st conv operation
+        :param act_type: Activation type to use
+        :param nb_input_channels: Number of input channels of the board representation
+        """
+
+        super(_Stem, self).__init__()
+
+        self.body = Sequential(
+            Conv2d(in_channels=nb_input_channels, out_channels=channels, kernel_size=(3, 3), padding=(1, 1),
+                   bias=False),
+            BatchNorm2d(num_features=channels),
+            get_act(act_type))
+
+    def forward(self, x):
+        """
+        Compute forward pass
+        :param F: Handle
+        :param x: Input data to the block
+        :return: Activation maps of the block
+        """
+        return self.body(x)
+
+
+class _PolicyHead(Module):
+    def __init__(self, board_height=11, board_width=11, channels=256, policy_channels=2, n_labels=4992, act_type="relu",
+                 select_policy_from_plane=False):
+        """
+        Definition of the policy head proposed by the alpha zero authors
+        :param policy_channels: Number of channels for 1st conv operation in branch 0
+        :param act_type: Activation type to use
+        channelwise squeeze excitation, channel-spatial-squeeze-excitation, respectively
+        """
+
+        super(_PolicyHead, self).__init__()
+
+        self.body = Sequential()
+        self.select_policy_from_plane = select_policy_from_plane
+
+        if self.select_policy_from_plane:
+            self.body = Sequential(Conv2d(in_channels=channels, out_channels=channels, padding=1, kernel_size=(3, 3), bias=False),
+                                   BatchNorm2d(num_features=channels),
+                                   get_act(act_type),
+                                   Conv2d(in_channels=channels, out_channels=policy_channels, padding=1, kernel_size=(3, 3), bias=False))
+            self.nb_flatten = policy_channels*board_width*policy_channels
+
+        else:
+            self.body = Sequential(Conv2d(in_channels=channels, out_channels=policy_channels, kernel_size=(1, 1), bias=False),
+                                   BatchNorm2d(num_features=policy_channels),
+                                   get_act(act_type))
+
+            self.nb_flatten = board_height*board_width*policy_channels
+            self.body2 = Sequential(Linear(in_features=self.nb_flatten, out_features=n_labels))
+
+    def forward(self, x):
+        """
+        Compute forward pass
+        :param x: Input data to the block
+        :return: Activation maps of the block
+        """
+        if self.select_policy_from_plane:
+            return self.body(x).view(-1, self.nb_flatten)
+        else:
+            x = self.body(x).view(-1, self.nb_flatten)
+            return self.body2(x)
