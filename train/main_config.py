fd61b2f5b1044c129a728e2c839a1e272a7dbfbd
anissa.manai <anissa.manai@stud.tu-darmstadt.de>
2022-09-08 17:44:07 +0200

    Update main_config

--- train/main_config.py
+++ train/main_config.py
@@ -1,5 +1,9 @@
 main_config = {
-    "train_dir": "/root/rbc/data/RBC-data-2021-train.hdf5",
-    "val_dir": "/root/rbc/data/RBC-data-2021-val.hdf5",
-    "test_dir": "/root/rbc/data/RBC-data-2021-test.hdf5"
+    "partial_train_dir": "/root/rbc/data/top3-partial-flip-train.hdf5",
+    "partial_val_dir": "/root/rbc/data/top3-partial-flip-val.hdf5",
+    "partial_test_dir": "/root/rbc/data/top3-partial-flip-test.hdf5", 
+
+    "train_dir": "/root/rbc/data/top3-flip-train.hdf5",
+    "val_dir": "/root/rbc/data/top3-flip-val.hdf5",
+    "test_dir": "/root/rbc/data/top3-flip-test.hdf5"
 }
