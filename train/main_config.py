68fe004e894b3ed2fae230bf7eba745c26531f63
anissa.manai <anissa.manai@stud.tu-darmstadt.de>
2022-09-25 17:07:31 +0200

    Fix pretraining

--- train/main_config.py
+++ train/main_config.py
@@ -5,5 +5,10 @@ main_config = {
 
     "train_dir": "/root/rbc/data/top3-flip-train.hdf5",
     "val_dir": "/root/rbc/data/top3-flip-val.hdf5",
-    "test_dir": "/root/rbc/data/top3-flip-test.hdf5"
+    "test_dir": "/root/rbc/data/top3-flip-test.hdf5", 
+
+
+    "train_dir_wh": "/root/rbc/data/top3-flip-train.hdf5",
+    "val_dir_wh": "/root/rbc/data/top3-flip-val.hdf5",
+    "test_dir_wh": "/root/rbc/data/top3-flip-test.hdf5"
 }
